c80ba7d168ec58a1b6e22710acf1a0cd3a967fa6
babastienne <potiron.bastien@gmail.com>
2016-04-26 14:27:20 +0200

    add the website structure

--- libs/test.py
+++ libs/test.py
@@ -1,8 +1,18 @@
+import bottle
 from bottle import route, template, run
 
-@route('/hello/<name>')
-def index(name):
-	return template('montemplate', blab=name)
+@route('/')
+@route('/index.html')
+def index():
+	return template('web/montemplate', { 'commune' : "void", 'activite' : "void", 'installation' : "void"})
 
 run(host='localhost', port=8080)
 
+
+
+@get('/action')
+def action():
+	commune = request.query.get('commune','void') #void correspond à la valeur par défaut si jamais le champ est vide (on aurai pu mettre 'blabla')
+	activite = request.query.get('activite','void')
+	installation = request.query.get('installation','void')
+	return template('web/montemplate', { 'commune' : commune, 'activite' : activite, 'installation' : installation})
