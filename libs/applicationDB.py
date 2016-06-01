8b1bbb626ae9acfa322365892789485a78b312ad
babastienne <potiron.bastien@gmail.com>
2016-06-01 13:43:41 +0200

    Update version

--- libs/applicationDB.py
+++ libs/applicationDB.py
@@ -1,26 +1,34 @@
-import dao
+import dao as database
+import extractcsv
 
-def __init__(self):
-    #TODO
+# def __init__(self):
+
 
 def initDataBase():
-    dao = coBD.Dao("julien", "julien", "pdl.db") # creation of the dataBase
+    dao = database.Dao("julien", "julien", "pdl.db") # creation of the dataBase
     dao.connexion() # connexion to the database
 
     dao.dropTables(["activite", "equipement", "installation"])
     dao.commit() # commit the modifications on the database
 
-    activite = ["activite", ["equipementid", "comLib", "actLib", "actCode", "actNivLib"], ["INTEGER", "TEXT", "TEXT", "INTEGER", "TEXT"]]
-    equipement = ["equipement", ["equipementid", "equNom", "comLib", "equipementTypeLib", "equipementFiche"], ["INTEGER", "TEXT", "TEXT", "TEXT", "TEXT"]]
-    installation = ["installation", ["nomInstallation", "numInstall", "nomCommune", "codeInsee", "codePostal"], ["TEXT", "TEXT", "TEXT", "TEXT", "TEXT"]]
+    activite = ["activite", ["equipementid", "comLib", "actLib", "actCode", "actNivLib"], ["INTEGER", "TEXT", "TEXT", "INTEGER", "TEXT"], "../datas/activite.csv"]
+    equipement = ["equipement", ["equipementid", "equNom", "comLib", "equipementTypeLib", "equipementFiche"], ["INTEGER", "TEXT", "TEXT", "TEXT", "TEXT"], "../datas/equipements.csv"]
+    installation = ["installation", ["nomInstallation", "numInstall", "nomCommune", "codeInsee", "codePostal"], ["TEXT", "TEXT", "TEXT", "TEXT", "TEXT"], "../datas/installations.csv"]
 
-    dao.createTable(activite[0], activite[1], activite[2])
-    dao.createTable(equipement[0], equipement[1], equipement[2])
-    dao.createTable(installation[0], installation[1], installation[2])
+    listeOfTables = [activite, equipement, installation]
 
-    dao.insertIntoTable(firstactivity)
+    for i in range(len(listeOfTables)):
+        dao.createTable(listeOfTables[i][0], listeOfTables[i][1], listeOfTables[i][2])
+
+    for i in range(len(listeOfTables)):
+        for object in extractcsv.getDataFromCSV(listeOfTables[i][3], listeOfTables[i][1], listeOfTables[i][0]):
+            dao.insertIntoTable(object)
 
     dao.commit() # commit the modifications on the database
     dao.deconnexion() # deconnexion of the database
 
+# def extractFromCSV(pathToCSVFile):
+
+
 initDataBase()
+# extractFromCSV("../datas/activite.csv")
--- libs/dao.py
+++ libs/dao.py
@@ -1,8 +1,5 @@
 import csv
 import sqlite3
-import classeActivite
-import classeEquipement
-import classeInstallation
 
 class Dao:
 
@@ -58,69 +55,65 @@ class Dao:
 		print(request)
 		self.connected.execute(request)
 
+	#
+	# ############################## OLD STUFF ###############################
+	#
+	# def importDataInTable(pathToCsvFile, fieldnames):
+	# 	with open(pathToCsvFile, 'r') as csvfile:
+	# 		myRead = csv.DictReader(csvfile, fieldnames=fieldnames)
+	# 		for row in myRead:
+	# 			myActivity = classeActivite.activite(row[fieldnames[0]],row[fieldnames[1]],row[fieldnames[2]],row[fieldnames[3]],row[fieldnames[4]])
+	# 			if myActivity.actLib != "" and myActivity.equipementid and myActivity.comLib and myActivity.actCode and myActivity.actNivLib:
+	# 				cursor.execute("INSERT INTO activite VALUES(?, ?, ?, ?, ? )", (myActivity.equipementid,myActivity.comLib,myActivity.actLib,myActivity.actCode,myActivity.actNivLib))
+	# 	csvfile.close()
+	#
+	#
+	# ################################################################# OLD ##########################
+	# with open('../datas/activite.csv', 'r') as csvfile:
+	#
 
-	############################## OLD STUFF ###############################
-
-	def importDataInTable(pathToCsvFile, fieldnames):
-		with open(pathToCsvFile, 'r') as csvfile:
-			myRead = csv.DictReader(csvfile, fieldnames=fieldnames)
-			for row in myRead:
-				boolean = true
-				for i in row:
-					if not i:
-						boolean = false
-				myActivity = classeActivite.activite(row[fieldnames[0]],row[fieldnames[1]],row[fieldnames[2]],row[fieldnames[3]],row[fieldnames[4]])
-				if myActivity.actLib != "" and myActivity.equipementid and myActivity.comLib and myActivity.actCode and myActivity.actNivLib:
-					cursor.execute("INSERT INTO activite VALUES(?, ?, ?, ?, ? )", (myActivity.equipementid,myActivity.comLib,myActivity.actLib,myActivity.actCode,myActivity.actNivLib))
-		csvfile.close()
-
-
-	################################################################# OLD ##########################
-	with open('../datas/activite.csv', 'r') as csvfile:
-
-
-	# Data import from activite.csv
-		fieldnames = ['ComInsee','comLib', 'Equipementid', 'EqunbEquIdentique', 'ActCode', 'ActLib', 'EquActivitePraticable','EquActivitePratique', 'EquActiviteSalleSpe', 'ActNivLib'] # On associe chaque colonne à un nom
-		myRead = csv.DictReader(csvfile, fieldnames=fieldnames)
-		accumulator = 0
-		for row in myRead:
-			if accumulator > 0:
-				myActivity = classeActivite.activite(row['Equipementid'],row['comLib'],row['ActLib'],row['ActCode'],row['ActNivLib'])
-				if myActivity.actLib != "" and myActivity.equipementid and myActivity.comLib and myActivity.actCode and myActivity.actNivLib:
-					cursor.execute("INSERT INTO activite(equipementid,comLib,actLib,actCode,actNivLib) VALUES(?, ?, ?, ?, ?)", (myActivity.equipementid,myActivity.comLib,myActivity.actLib,myActivity.actCode,myActivity.actNivLib))
-			accumulator = accumulator + 1
-		csvfile.close()
-	# End data import from activite.csv
-
-	# Data import from equipement.csv
-	with open('../datas/equipements.csv', 'r') as csvfile:
-		conn = sqlite3.connect('ma_base.db') # Permet de créer la base de donnée
-		cursor = conn.cursor()
-		fieldnames2 = ['ComInsee','ComLib', 'InsNumeroInstall', 'InsNom', 'EquipementId', 'EquNom', 'EquNomBatiment','EquipementTypeLib', 'EquipementFiche'] # On associe chaque colonne à un nom
-		myRead = csv.DictReader(csvfile, fieldnames=fieldnames2)
-		accumulator = 0
-
-		for row in myRead:
-			if accumulator > 0:
-				myEquipement = classeEquipement.equipement(row['EquipementId'],row['EquNom'],row['ComLib'],row['EquipementTypeLib'],row['EquipementFiche'])
-				if myEquipement.equipementid != "" and myEquipement.equNom and myEquipement.comLib and myEquipement.equipementTypeLib and myEquipement.equipementFiche:
-					cursor.execute("INSERT INTO equipement(equipementid,equNom,comLib,equipementTypeLib,equipementFiche) VALUES(?, ?, ?, ?, ?)", (myEquipement.equipementid, myEquipement.equNom, myEquipement.comLib, myEquipement.equipementTypeLib,myEquipement.equipementFiche))
-			accumulator = accumulator + 1
-		csvfile.close()
-	# End data import from equipement.csv
-
-	# Data import from installation.csv
-	with open('../datas/installations.csv', 'r') as csvfile:
-		conn = sqlite3.connect('ma_base.db') # Permet de créer la base de donnée
-		cursor = conn.cursor()
-		fieldnames3 = ['Nom usuel de l\'installation','Numero de l\'installation', 'Nom de la commune', 'Code INSEE', 'Code postal'] # On associe chaque colonne à un nom
-		myRead = csv.DictReader(csvfile, fieldnames=fieldnames3)
-		accumulator = 0
-		for row in myRead:
-			if accumulator > 0:
-				myInstallation = classeInstallation.installation(row['Nom usuel de l\'installation'],row['Numero de l\'installation'],row['Nom de la commune'],row['Code INSEE'],row['Code postal'])
-				if myInstallation.nomInstallation != "" and myInstallation.numInstall and myInstallation.nomCommune and myInstallation.codeInsee and myInstallation.codePostal:
-					cursor.execute("INSERT INTO installation(nomInstallation,numInstall,nomCommune,codeInsee,codePostal) VALUES(?, ?, ?, ?, ?)", (myInstallation.nomInstallation, myInstallation.numInstall, myInstallation.nomCommune, myInstallation.codeInsee,myInstallation.codePostal))
-			accumulator = accumulator + 1
-		csvfile.close()
+	# # Data import from activite.csv
+	# 	fieldnames = ['ComInsee','comLib', 'Equipementid', 'EqunbEquIdentique', 'ActCode', 'ActLib', 'EquActivitePraticable','EquActivitePratique', 'EquActiviteSalleSpe', 'ActNivLib'] # On associe chaque colonne à un nom
+	# 	myRead = csv.DictReader(csvfile, fieldnames=fieldnames)
+	# 	accumulator = 0
+	# 	for row in myRead:
+	# 		if accumulator > 0:
+	# 			myActivity = classeActivite.activite(row['Equipementid'],row['comLib'],row['ActLib'],row['ActCode'],row['ActNivLib'])
+	# 			if myActivity.actLib != "" and myActivity.equipementid and myActivity.comLib and myActivity.actCode and myActivity.actNivLib:
+	# 				cursor.execute("INSERT INTO activite(equipementid,comLib,actLib,actCode,actNivLib) VALUES(?, ?, ?, ?, ?)", (myActivity.equipementid,myActivity.comLib,myActivity.actLib,myActivity.actCode,myActivity.actNivLib))
+	# 		accumulator = accumulator + 1
+	# 	csvfile.close()
+	# # End data import from activite.csv
+	#
+	# # Data import from equipement.csv
+	# with open('../datas/equipements.csv', 'r') as csvfile:
+	# 	conn = sqlite3.connect('ma_base.db') # Permet de créer la base de donnée
+	# 	cursor = conn.cursor()
+	# 	fieldnames2 = ['ComInsee','ComLib', 'InsNumeroInstall', 'InsNom', 'EquipementId', 'EquNom', 'EquNomBatiment','EquipementTypeLib', 'EquipementFiche'] # On associe chaque colonne à un nom
+	# 	myRead = csv.DictReader(csvfile, fieldnames=fieldnames2)
+	# 	accumulator = 0
+	#
+	# 	for row in myRead:
+	# 		if accumulator > 0:
+	# 			myEquipement = classeEquipement.equipement(row['EquipementId'],row['EquNom'],row['ComLib'],row['EquipementTypeLib'],row['EquipementFiche'])
+	# 			if myEquipement.equipementid != "" and myEquipement.equNom and myEquipement.comLib and myEquipement.equipementTypeLib and myEquipement.equipementFiche:
+	# 				cursor.execute("INSERT INTO equipement(equipementid,equNom,comLib,equipementTypeLib,equipementFiche) VALUES(?, ?, ?, ?, ?)", (myEquipement.equipementid, myEquipement.equNom, myEquipement.comLib, myEquipement.equipementTypeLib,myEquipement.equipementFiche))
+	# 		accumulator = accumulator + 1
+	# 	csvfile.close()
+	# # End data import from equipement.csv
+	#
+	# # Data import from installation.csv
+	# with open('../datas/installations.csv', 'r') as csvfile:
+	# 	conn = sqlite3.connect('ma_base.db') # Permet de créer la base de donnée
+	# 	cursor = conn.cursor()
+	# 	fieldnames3 = ['Nom usuel de l\'installation','Numero de l\'installation', 'Nom de la commune', 'Code INSEE', 'Code postal'] # On associe chaque colonne à un nom
+	# 	myRead = csv.DictReader(csvfile, fieldnames=fieldnames3)
+	# 	accumulator = 0
+	# 	for row in myRead:
+	# 		if accumulator > 0:
+	# 			myInstallation = classeInstallation.installation(row['Nom usuel de l\'installation'],row['Numero de l\'installation'],row['Nom de la commune'],row['Code INSEE'],row['Code postal'])
+	# 			if myInstallation.nomInstallation != "" and myInstallation.numInstall and myInstallation.nomCommune and myInstallation.codeInsee and myInstallation.codePostal:
+	# 				cursor.execute("INSERT INTO installation(nomInstallation,numInstall,nomCommune,codeInsee,codePostal) VALUES(?, ?, ?, ?, ?)", (myInstallation.nomInstallation, myInstallation.numInstall, myInstallation.nomCommune, myInstallation.codeInsee,myInstallation.codePostal))
+	# 		accumulator = accumulator + 1
+	# 	csvfile.close()
 	# End fin gestion fichier installation
